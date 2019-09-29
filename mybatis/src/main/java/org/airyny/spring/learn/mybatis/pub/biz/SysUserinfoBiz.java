0792b6b15c0444ef9107c08d29fa4b088f9eda4e
向永烨 <xiang.yongye@hand-china.com>
2019-09-29 19:31:49 +0800

    mybatis

---
+++ mybatis/src/main/java/org/airyny/spring/learn/mybatis/pub/biz/SysUserinfoBiz.java
@@ -0,0 +1,26 @@
+package org.airyny.spring.learn.mybatis.pub.biz;
+
+import java.util.List;
+
+import org.airyny.spring.learn.mybatis.pub.dao.dev.SysUserinfoMapper;
+import org.airyny.spring.learn.mybatis.pub.model.dev.SysUserinfo;
+import org.airyny.spring.learn.mybatis.pub.model.dev.SysUserinfoExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: yongye(xiang.yongye @ hand - china.com)
+ * @Date:2019/9/29 17:23
+ * @Version:1.0
+ * @deseription:
+ **/
+@Service("sysUserinfoBiz")
+public class SysUserinfoBiz {
+
+    @Autowired
+    SysUserinfoMapper sysUserinfoMapper;
+
+    public List<SysUserinfo> getList(){
+        return sysUserinfoMapper.selectByExample(new SysUserinfoExample());
+    }
+}
---
+++ mybatis/src/test/java/mybatis/BizTest.java
@@ -0,0 +1,22 @@
+package mybatis;
+
+import org.airyny.spring.learn.mybatis.pub.biz.SysUserinfoBiz;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author: yongye(xiang.yongye @ hand - china.com)
+ * @Date:2019/9/29 17:21
+ * @Version:1.0
+ * @deseription:
+ **/
+public class BizTest {
+    ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-context.xml");
+
+    @Test
+    public void bizTest(){
+        SysUserinfoBiz sysUserinfoBiz = context.getBean("sysUserinfoBiz",SysUserinfoBiz.class);
+        System.out.println(sysUserinfoBiz.getList().toString());
+    }
+}
